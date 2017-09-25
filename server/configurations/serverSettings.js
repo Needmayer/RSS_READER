b4e623e0f6bbb60aef14b18af3b92d325d4b19ad
Lukas <lukas.hodan1@gmail.com>
2017-09-25 14:29:21 +0200

    config

--- server/configurations/serverSettings.js
+++ server/configurations/serverSettings.js
@@ -1,5 +1,5 @@
 export function getMongoConnection(){
-    if(process.env.NODE_ENV !== 'production'){
+    if(process.env.NODE_ENV === 'production'){
         return 'mongodb://Needmayer:o4Lh7jJP@ds119508.mlab.com:19508/needdb';
     }else {
         return 'mongodb://localhost:3000/rss_reader';
